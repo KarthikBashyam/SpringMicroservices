4e65b6bec9aba3fb8ff15b0a77f724c170307a84
Karthik <karthik@email.com>
2018-10-14 18:26:35 -0400

    Committed api gateway load balancer changes

--- service-api-gateway/src/main/java/com/client/serviceapigateway/ServiceApiGatewayApplication.java
+++ service-api-gateway/src/main/java/com/client/serviceapigateway/ServiceApiGatewayApplication.java
@@ -2,21 +2,30 @@ package com.client.serviceapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * Api Gateway application
+ * 
+ * @author Karthik
+ *
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApiGatewayApplication.class, args);
 	}
-	
+
 	@Bean
 	RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(r -> r.path("/orders").uri("http://localhost:10002/orders/list"))
+				.route(r -> r.path("/orders/list").uri("lb://order-service"))
+				.route(r -> r.path("/products/list").uri("lb://product-service"))
 				.build();
 	}
 }
