2e24146907bb3d36f304fadbe77f0c39279c58a3
Karthik <karthik@email.com>
2018-10-14 19:26:18 -0400

    Renamed failure analyzer class

--- application-spring-boot-starter/src/main/java/com/application/common/exception/ConfigNotFoundFailureAnalyzer.java
+++ application-spring-boot-starter/src/main/java/com/application/common/exception/ConfigNotFoundFailureAnalyzer.java
@@ -3,7 +3,7 @@ package com.application.common.exception;
 import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
 import org.springframework.boot.diagnostics.FailureAnalysis;
 
-public class ApplicationFailureAnalyzer extends AbstractFailureAnalyzer<ConfigFileNotFoundException> {
+public class ConfigNotFoundFailureAnalyzer extends AbstractFailureAnalyzer<ConfigFileNotFoundException> {
 
 	@Override
 	protected FailureAnalysis analyze(Throwable rootFailure, ConfigFileNotFoundException cause) {
