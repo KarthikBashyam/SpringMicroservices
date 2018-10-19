1c5ac9fcf64b2d07fa30f63d45412996303314f2
Karthik <karthik@email.com>
2018-10-18 20:39:36 -0400

    updated config

--- application-spring-boot-starter/src/main/java/com/application/common/health/ApplicationHealthIndicator.java
+++ application-spring-boot-starter/src/main/java/com/application/common/health/ApplicationHealthIndicator.java
@@ -3,14 +3,28 @@ package com.application.common.health;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @ConditionalOnProperty("application.health.enabled")
-public class ApplicationHealthIndicator implements HealthIndicator {
+@Component
+public class ApplicationHealthIndicator implements HealthIndicator, EnvironmentAware {
+
+	private Environment environment;
 
 	@Override
 	public Health health() {
-		// TODO Auto-generated method stub
-		return null;
+		if (environment.getProperty("spring.application.name") == null)
+			return Health.down().withDetail("config", "Application name not defined").build();
+		else
+			return Health.up().withDetail("config", "Application name is defined").build();
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+
 	}
 
 }
